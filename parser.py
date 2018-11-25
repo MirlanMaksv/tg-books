5cfa7641d4b97aa1880c4531eb5c31a71883a8e9
Mirlan <m.maksitaliev@gmail.com>
2018-11-25 23:43:45 +0600

    done book downloading

---
+++ parser.py
@@ -0,0 +1,21 @@
+import os
+import argparse
+
+CHANNEL = "python_textbooks"
+KEY_WORDS = ["Computer vision", "Machine learning"]
+EXTENSIONS = [".pdf"]
+
+
+descr = "Book downloader from telegram chats"
+parser = argparse.ArgumentParser(description=descr)
+
+parser.add_argument("-c", "--channel", type=str, default=CHANNEL, required=False,
+                    help="Channel to search in")
+parser.add_argument("-kw", "--key-words", nargs="+", default=KEY_WORDS, required=False,
+                    help="Key words to search, ex: 'Computer vision' 'Machine learning'")
+parser.add_argument("-e", "--extensions", nargs="+", required=False,
+                    help="File extensions")
+
+args = parser.parse_args()
+args.key_words = list(map(lambda x: x.lower(), args.key_words))
+args.path = args.channel + "-downloads/"
