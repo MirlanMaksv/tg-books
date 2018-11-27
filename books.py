929614ce02bcc64f8b17d61124ba8e1d90359866
Mirlan <m.maksitaliev@gmail.com>
2018-11-27 13:06:17 +0600

    added --action

---
+++ books.py
@@ -0,0 +1,61 @@
+import os
+from telethon import TelegramClient, sync, utils
+from telethon.tl.types import InputMessagesFilterDocument
+from parser import args
+
+
+def filter_by_name(doc):
+    doc_name = get_name(doc).lower()
+    mapped = map(lambda key_word: key_word in doc_name, args.key_words)
+    return any(mapped)
+
+
+def filter_by_ext(doc):
+    return get_extension(doc) in args.extensions
+
+
+def get_extension(doc):
+    return os.path.splitext(get_name(doc))[1]
+
+
+def get_name(doc):
+    if doc.media.document and doc.media.document.attributes \
+            and doc.media.document.attributes[0]:
+        return doc.media.document.attributes[0].file_name
+
+    return ""
+
+
+def handle():
+    client = TelegramClient("sessions", API_ID, API_KEY)
+
+    try:
+        client.start()
+
+        docs = client.get_messages(args.channel, None, filter=InputMessagesFilterDocument)
+        books = filter(filter_by_name, docs)
+        if args.extensions:
+            books = list(filter(filter_by_ext, books))
+
+        total = len(books)
+        print("{} books found".format(total))
+
+        for i, book in enumerate(books):
+            book_name = get_name(book)
+            if args.action == "list":
+                print(i+1, book_name)
+
+            elif args.action == "download":
+                print("Downloading {index} of {total} | {name}".format(index=i+1, total=total, name=book_name))
+
+                book.download_media(args.path + book_name)
+
+    finally:
+        client.disconnect()
+
+
+API_ID = os.environ["TG_API_ID"]
+API_KEY = os.environ["TG_API_HASH"]
+
+if __name__ == "__main__":
+    handle()
--- parser.py
+++ parser.py
@@ -9,6 +9,8 @@ EXTENSIONS = [".pdf"]
 descr = "Book downloader from telegram chats"
 parser = argparse.ArgumentParser(description=descr)
 
+parser.add_argument("-a", "--action", type=str, required=True,
+                    help="Download books or just list their names")
 parser.add_argument("-c", "--channel", type=str, default=CHANNEL, required=False,
                     help="Channel to search in")
 parser.add_argument("-kw", "--key-words", nargs="+", default=KEY_WORDS, required=False,
